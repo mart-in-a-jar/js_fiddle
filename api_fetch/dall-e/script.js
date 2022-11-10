4b28fc1d12fcf28c73035b72058d21df85239df8
Martin laptop <Martin.Andre.Jarneid@ssn.no>
2022-11-10 12:09:01 +0100

    Add error handling for bad requests

--- api_fetch/dall-e/script.js
+++ api_fetch/dall-e/script.js
@@ -21,11 +21,20 @@ async function generateImage(prompt, number, size) {
         const response = await fetch(req);
         const data = await response.json();
         if (!response.ok) {
+            if (response.status === 400) {
+                return {
+                    status: "failed",
+                    error_message: data.error.message,
+                };
+            }
             throw new Error("Http request failed");
         }
-        return data.data[0].url;
+        return {
+            status: "success",
+            url: data.data[0].url,
+        };
     } catch (error) {
-        console.error("Something went wrong: ", error);
+        console.error("Something went wrong: ", error, error.data);
     }
 }
 
@@ -34,8 +43,16 @@ const searchField = document.querySelector("input");
 const seachButton = document.querySelector("button");
 
 async function placeImage(prompt) {
+    document.querySelector(".error").textContent = "";
     image.src = "";
-    image.src = await generateImage(prompt, 1, "1024x1024");
+    document.querySelector(".img-placeholder").style.display = "block";
+    let response = await generateImage(prompt, 1, "1024x1024");
+    if (response.status === "failed") {
+        document.querySelector(".error").textContent = response.error_message;
+    } else if (response.status === "success") {
+        image.src = response.url;
+    }
+    document.querySelector(".img-placeholder").style.display = "none";
 }
 
 searchField.addEventListener("keydown", (e) => {
