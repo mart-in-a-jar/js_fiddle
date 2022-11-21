0c18a3ad0675bfa0c7d3e6a62f9599fbadac402d
Martin laptop <Martin.Andre.Jarneid@ssn.no>
2022-11-21 14:51:41 +0100

    Upload beginning of exercise

---
+++ data_structure/linkedlist.js
@@ -0,0 +1,118 @@
+const linkedlist = () => {
+    let head = null;
+
+    function Node(value = null) {
+        this.value = value;
+        this.next = null;
+    }
+
+    const append = (value) => {
+        const newNode = new Node(value);
+        if (!head) {
+            return (head = newNode);
+        }
+        let nextNode = head;
+        while (nextNode.next != null) {
+            nextNode = nextNode.next;
+        }
+        return (nextNode.next = newNode);
+    };
+
+    const prepend = (value) => {
+        const newNode = new Node(value);
+        if (!head) {
+            return (head = newNode);
+        }
+        newNode.next = head;
+        return (head = newNode);
+    };
+
+    const size = () => {
+        let count = 0;
+        let nextNode = head;
+        while (nextNode != null) {
+            nextNode = nextNode.next;
+            count++;
+        }
+        return count;
+    };
+
+    const getHead = () => {
+        if (!head) return null;
+        return head.value;
+    };
+
+    const tail = () => {
+        if (!head) return null;
+        let nextNode = head;
+        while (nextNode.next != null) {
+            nextNode = nextNode.next;
+        }
+        return nextNode.value;
+    };
+
+    const at = (index) => {
+        if (!head || index > size() - 1) return null;
+        let nextNode = head;
+        for (let i = 0; i < index; i++) {
+            nextNode = nextNode.next;
+        }
+        return nextNode.value;
+    };
+
+    const toString = () => {
+        if (!head) return null;
+        let string = `( ${head.value} )`;
+        let nextNode = head;
+        while (nextNode.next != null) {
+            nextNode = nextNode.next;
+            string += ` -> ( ${nextNode.value} )`;
+        }
+        string += ` -> ${nextNode.next}`;
+        return string;
+    };
+
+    return {
+        append,
+        prepend,
+        size,
+        head: getHead,
+        tail,
+        at,
+        toString,
+    };
+};
+
+const a = linkedlist();
+console.log("AT 2 ", a.at(2));
+console.log(a.size());
+console.log("Head: ", a.head());
+console.log("Tail: ", a.tail());
+a.prepend("1");
+console.log(a.size());
+console.log(a.toString());
+console.log("Head: ", a.head());
+console.log("Tail: ", a.tail());
+console.log("AT 0 ", a.at(0));
+console.log("AT 1 ", a.at(1));
+a.append("2");
+console.log(a.size());
+console.log(a.toString());
+console.log("AT 2 ", a.at(2));
+a.append("3");
+console.log(a.size());
+console.log(a.toString());
+a.append("4");
+console.log(a.size());
+console.log(a.toString());
+a.prepend("0");
+console.log(a.size());
+console.log(a.toString());
+a.prepend("-1");
+console.log(a.size());
+console.log(a.toString());
+console.log("Head: ", a.head());
+console.log("Tail: ", a.tail());
+console.log("AT 2 ", a.at(2));
+console.log("AT 6 ", a.at(6));
+console.log("AT 8 ", a.at(8));
