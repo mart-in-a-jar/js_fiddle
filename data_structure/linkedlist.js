12e71f920d9fa910d3d3ef99cfa6812a11d3b8ca
Martin laptop <Martin.Andre.Jarneid@ssn.no>
2022-11-24 10:58:21 +0100

    Add contains and find function

--- data_structure/linkedlist.js
+++ data_structure/linkedlist.js
@@ -72,6 +72,30 @@ const linkedlist = () => {
         return (nextNode.next = null);
     };
 
+    const contains = (value) => {
+        if (!head) return false;
+        let nextNode = head;
+        while (nextNode != null) {
+            if (nextNode.value === value) return true;
+            nextNode = nextNode.next;
+        }
+        return false;
+    };
+
+    const find = (value) => {
+        if (!head) return null;
+        let index = 0;
+        let nextNode = head;
+        while (nextNode != null) {
+            if (nextNode.value === value) return index;
+            else {
+                nextNode = nextNode.next;
+                index++;
+            }
+        }
+        return null;
+    };
+
     const toString = () => {
         if (!head) return null;
         let string = `( ${head.value} )`;
@@ -92,11 +116,15 @@ const linkedlist = () => {
         tail,
         at,
         pop,
+        contains,
+        find,
         toString,
     };
 };
 
 const a = linkedlist();
+console.log("Contains 1 ", a.contains("1"));
+console.log("Find 1 ", a.find("1"));
 console.log("AT 2 ", a.at(2));
 console.log(a.size());
 console.log("Head: ", a.head());
@@ -109,6 +137,8 @@ console.log("Head: ", a.head());
 console.log("Tail: ", a.tail());
 console.log("AT 0 ", a.at(0));
 console.log("AT 1 ", a.at(1));
+console.log("Contains 1 ", a.contains("1"));
+console.log("Find 1 ", a.find("1"));
 console.log("POP ", a.pop());
 console.log(a.size());
 console.log(a.toString());
@@ -116,12 +146,14 @@ a.append("2");
 console.log(a.size());
 console.log(a.toString());
 console.log("AT 2 ", a.at(2));
+console.log("Contains 2 ", a.contains("2"));
 a.append("3");
 console.log(a.size());
 console.log(a.toString());
 a.append("4");
 console.log(a.size());
 console.log(a.toString());
+console.log("Find 3 ", a.find("3"));
 a.prepend("0");
 console.log(a.size());
 console.log(a.toString());
@@ -134,6 +166,10 @@ console.log("AT 2 ", a.at(2));
 console.log("AT 5 ", a.at(5));
 console.log("AT 8 ", a.at(8));
 console.log(a.toString());
+console.log("Contains -1 ", a.contains("-1"));
+console.log("Contains 4 ", a.contains("4"));
+console.log("Find 0 ", a.find("0"));
+console.log("Find 8 ", a.find("8"));
 console.log(a.pop());
 console.log(a.size());
 console.log(a.toString());
