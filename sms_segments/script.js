9f4d17bf9d2c3ee63d8c3c62273d8078b9a66ece
Martin <Martin.Andre.Jarneid@ssn.no>
2024-08-30 15:21:35 +0200

    Add style, images and get change rate from api

--- sms_segments/script.js
+++ sms_segments/script.js
@@ -5,11 +5,29 @@ const textInput = document.getElementById("sms-body");
 const encodingOutput = document.getElementById("encoding");
 const segmentOutput = document.getElementById("segments");
 const priceOutput = document.getElementById("price");
-const changeRate = 10.83;
-const smsPrice = 0.062; // $
+const img = document.querySelector(".woah");
+let changeRate = 10.83;
+const smsPrice = 0.0651; // $
 
 let smsBody;
 let segmentedMessage;
+
+const getExchangerate = async () => {
+    try {
+        const res = await fetch(
+            "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json",
+        );
+        const data = await res.json();
+        changeRate = data.usd.nok;
+        if (res.ok) {
+            console.log(`Exchange rate: ${changeRate}`);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+getExchangerate();
 
 function getPrice(segments) {
     const priceUS = segments * smsPrice;
@@ -24,7 +42,10 @@ textInput.addEventListener("input", () => {
     const price = getPrice(numberOfSegments);
     encodingOutput.textContent = segmentedMessage.encodingName;
     segmentOutput.textContent = numberOfSegments;
-    priceOutput.textContent =
-        `${Math.round(price.priceNO * 100) / 100}kr / $${Math.round(price.priceUS * 100) / 100}`;
-    console.log(price);
+    priceOutput.textContent = `${Math.round(price.priceNO * 100) / 100}kr ($${Math.round(price.priceUS * 100) / 100})`;
+    if (numberOfSegments > 1) {
+        img.style.display = "block";
+    } else {
+        img.style.display = "none";
+    }
 });
